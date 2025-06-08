69c35c57b8ecdf0a320badfe063ac6e892545007
JoeTheHammer <hammererjohannes@gmail.com>
2025-06-08 13:53:30 +0200

    Fixed wrong import

--- src/controllers/sac_controller.py
+++ src/controllers/sac_controller.py
@@ -1,6 +1,6 @@
 from typing import Any
 
-import gym
+import gymnasium
 from stable_baselines3 import SAC
 
 from controllers.base_controller import IController
@@ -10,22 +10,21 @@ from custom_loggers.experiment_logger import logger
 
 class SACController(IController):
 
-    def __init__(self, model, env: gym.Env, **kwargs: Any):
+    def __init__(self, model, env: gymnasium.Env, **kwargs: Any):
         logger.info("Initializing SAC controller")
         super().__init__(env, **kwargs)
         self.model = model
 
         logger.info("Train SAC controller")
-        self.model.learn(total_timesteps=50000)
+        self.model.learn(total_timesteps=50)
 
     def get_action(self, state: Any) -> Any:
         action, _ = self.model.predict(state)
-        print(action)
         return action
 
 
 class SACProvider(IControllerProvider):
-    def create_controller(self, env: gym.Env, config_path: str | None = None) -> SACController:
+    def create_controller(self, env: gymnasium.Env, config_path: str | None = None) -> SACController:
         # Communicate to env that SAC support only continuous action space.
         env.continuous_action_space = True
 
