2954428266731d0cd87a5f48e825273d023a9125
JoeTheHammer <hammererjohannes@gmail.com>
2025-06-08 17:35:06 +0200

    Added testcase

--- tests/reward/test_expression_reward.py
+++ tests/reward/test_expression_reward.py
@@ -45,8 +45,19 @@ def test_action_penalty_in_expression():
 def test_combined_temp_and_action_expression():
     reward_fn = ExpressionReward(
         expression="(2 * exp(-((air_temp_101 - target_temp)**2) / (2 * sigma**2)) - 1) - abs(action - target_temp) * 0.1",
-        params={"target_temp": 23.0, "sigma": 1.0, },
+        params={
+            "target_temp": 23.0,
+            "sigma": 1.0,
+        },
     )
     reward, _ = reward_fn({"air_temp_101": 23.0, "action": 25.0, "__compute_reward__": True})
     expected = (2 * np.exp(-((23.0 - 23.0) ** 2) / (2 * 1.0**2)) - 1) - abs(25.0 - 23.0) * 0.1
+    assert abs(reward - expected) < 1e-6
+
+
+def test_use_one_part_of_dict():
+    reward_fn = ExpressionReward(expression="2 * day_sin", params={})
+    reward, _ = reward_fn({"day_sin": 20.0, "__compute_reward__": True})
+    expected = 2 * 20.0
+
     assert abs(reward - expected) < 1e-6
