e6f0343096af320c0540a42186c999d893d38692
JoeTheHammer <hammererjohannes@gmail.com>
2025-08-16 14:12:32 +0200

    Fixed logging for discrete action wrapper

--- src/wrappers/discrete_action_wrapper.py
+++ src/wrappers/discrete_action_wrapper.py
@@ -1,6 +1,7 @@
 import math
 
 import gymnasium as gym
+import numpy as np
 from gymnasium.spaces import Discrete, Tuple
 
 
@@ -15,8 +16,9 @@ class DiscreteActionWrapper(gym.ActionWrapper):
 
         # Ensure the original action space is the expected type
         assert isinstance(env.action_space, Tuple), "Action space must be a Tuple"
-        assert all(isinstance(space, Discrete) for space in
-                   env.action_space), "Tuple must contain only Discrete spaces"
+        assert all(
+            isinstance(space, Discrete) for space in env.action_space
+        ), "Tuple must contain only Discrete spaces"
 
         # Calculate the size of the new combined discrete space
         self.discrete_sizes = [space.n for space in env.action_space]
@@ -50,3 +52,28 @@ class DiscreteActionWrapper(gym.ActionWrapper):
             action_index += action_tuple[i] * current_product
             current_product *= self.discrete_sizes[i]
         return action_index
+
+    def get_energy_plus_action(self, action_index: int) -> np.ndarray:
+        """
+        Gets the individual discrete actions (as a tuple of integers)
+        and converts them to the corresponding real-valued actions.
+
+        Note: This method is specifically designed for use in EnergyPlus environments
+        that utilize a `custom_action_space` object with a `to_eplus_action` method. If this
+        is not present, the passed action is returned.
+
+        Args:
+            action_index (int): The single integer action from the agent.
+
+        Returns:
+            np.ndarray: A flat NumPy array of real actuator values.
+        """
+        # First, map the agent's single action to the tuple of discrete actions
+        discrete_actions_tuple = self.action(action_index)
+
+        # Then, use the wrapped environment's method to convert the tuple
+        # of discrete indices into real-valued actions.
+        if hasattr(self.unwrapped, "custom_action_space"):
+            return self.unwrapped.custom_action_space.to_eplus_action(discrete_actions_tuple)
+        else:
+            return discrete_actions_tuple
