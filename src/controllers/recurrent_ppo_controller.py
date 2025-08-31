ad1f1698ed666d7ca0568d7b43bbac806c090fd4
JoeTheHammer <hammererjohannes@gmail.com>
2025-08-31 16:33:02 +0200

    Updated recurrent ppo controller

--- src/controllers/recurrent_ppo_controller.py
+++ src/controllers/recurrent_ppo_controller.py
@@ -1,45 +1,18 @@
 from typing import (
-    Any,
     Dict,
 )
 
 import gymnasium as gym
 from sb3_contrib import RecurrentPPO
-from stable_baselines3.common.monitor import Monitor
 
 from adapters.on_policy_vec_env import OnPolicyAdapter
 from controllers.base_controller import ControllerSetup
 from controllers.base_rl_controller import (
-    IRLController,
-    IRLControllerFactory,
-    load_rl_controller_config,
+    IRLControllerFactory, load_rl_controller_config,
 )
 from controllers.utils import add_squash_output_to_hp, stabilize_training
-from environments.base_factory import IEnvironmentFactory
-
-
-class PPOController(IRLController):
-
-    def __init__(self, env: gym.Env, params: Dict):
-        super().__init__(env)
-
-        env = Monitor(env)
-
-        self.model = RecurrentPPO(
-            "MlpLstmPolicy",
-            env,
-            verbose=0,
-            use_sde=True,
-            policy_kwargs=dict(squash_output=True),
-            **params,
-        )
-
-    def get_action(self, state: Any) -> Any:
-        action, _ = self.model.predict(state)
-        return action
-
-    def train(self, timesteps: int):
-        self.model.learn(timesteps)
+from wrappers.manager import EnvWrapperManager
+from wrappers.continuous_action_wrapper import ContinuousActionWrapper
 
 
 class RecurrentPPOFactory(IRLControllerFactory):
@@ -48,25 +21,23 @@ class RecurrentPPOFactory(IRLControllerFactory):
         hyper_params = add_squash_output_to_hp(hyper_params)
         hyper_params = stabilize_training(hyper_params)
 
-        normalize_reward = kwargs.get("normalize_reward", False)
-        report_denormalized_state = kwargs.get("report_denormalized_state", False)
+        training_config = load_rl_controller_config(self.config_path).training
 
         return OnPolicyAdapter(
             env=env,
             model_class=RecurrentPPO,
             hyperparams=hyper_params,
-            normalize_reward=normalize_reward,
-            report_denormalized_state=report_denormalized_state,
+            report_denormalized_state=training_config.report_denormalized_state,
             normalize_action=True,
             policy="MlpLstmPolicy",
         )
-        # return PPOController(env, hyper_params)
 
     def create_controller_setup(self) -> ControllerSetup:
         if self.config_path is None or self.config_path == "":
             raise RuntimeError("No configuration was provided for the PPO controller.")
 
-        return super().create_rl_controller_setup(
-            is_continuous_action_space=True,
-            on_policy=True,
-        )
+        rl_config = load_rl_controller_config(self.config_path)
+        env_wrap_manager = EnvWrapperManager([ContinuousActionWrapper], rl_config.environment_wrapper)
+        hp = rl_config.hyperparameters
+
+        return super().create_rl_controller_setup_new(hp, env_wrap_manager, is_env_adapter=True)
