4211d9802e5f83bcf53d5dc2371cb89de3431658
JoeTheHammer <hammererjohannes@gmail.com>
2025-09-13 11:30:11 +0200

    Added intitial frontend application

---
+++ frontend/src/App.tsx
@@ -0,0 +1,23 @@
+function App() {
+
+  return (
+    <>
+      <div>
+        <div className="bg-red-200">Welcome to the App</div>
+      </div>
+    </>
+  )
+}
+
+
+export default App
+
+
+
+
+
+
+
+
+
+
