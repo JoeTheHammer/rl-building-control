5d97221ca7b7d54625551557b0a90b9a409e5fec
JoeTheHammer <hammererjohannes@gmail.com>
2025-09-28 10:35:30 +0200

    Added service to load configs

--- backend/src/api/controller.py
+++ backend/src/api/controller.py
@@ -6,11 +6,29 @@ from services.yaml_controller import save_controller
 
 router = APIRouter()
 
+BASE_DIR = Path(__file__).resolve().parents[3]
+CONTROLLERS_DIR = BASE_DIR / "config" / "controllers"
+
 @router.post("/save")
 def save_ctrl(req: SaveControllerRequest):
     try:
         filepath = Path(req.directory) / req.filename
         save_controller(req)
         return {"saved": True, "path": str(filepath.resolve())}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+
+@router.get("/all")
+def get_all_controller_configs():
+    try:
+        if not CONTROLLERS_DIR.exists() or not CONTROLLERS_DIR.is_dir():
+            raise HTTPException(
+                status_code=404,
+                detail=f"Folder not found: {CONTROLLERS_DIR}"
+            )
+
+        files = [f.name for f in CONTROLLERS_DIR.glob("*.yaml")]
+        return {"files": files}
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
--- backend/src/api/environment.py
+++ backend/src/api/environment.py
@@ -6,11 +6,28 @@ from services.yaml_env import save_environment_yaml
 
 router = APIRouter()
 
+BASE_DIR = Path(__file__).resolve().parents[3]
+ENVIRONMENTS_DIR = BASE_DIR / "config" / "environments"
+
 @router.post("/save")
 def save_env(req: SaveEnvironmentRequest):
     try:
         filepath = Path(req.directory) / req.filename
         save_environment_yaml(req.config, filepath)
         return {"saved": True, "path": str(filepath.resolve())}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+@router.get("/all")
+def get_all_environment_configs():
+    try:
+        if not ENVIRONMENTS_DIR.exists() or not ENVIRONMENTS_DIR.is_dir():
+            raise HTTPException(
+                status_code=404,
+                detail=f"Folder not found: {ENVIRONMENTS_DIR}"
+            )
+
+        files = [f.name for f in ENVIRONMENTS_DIR.glob("*.yaml")]
+        return {"files": files}
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
