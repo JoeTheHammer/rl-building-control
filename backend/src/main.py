1bb17ba107b1981f36024fff0dd0b333d78061b9
JoeTheHammer <hammererjohannes@gmail.com>
2025-11-10 19:54:58 +0100

    Updated CORS origins

--- backend/src/main.py
+++ backend/src/main.py
@@ -10,9 +10,15 @@ from api import analytics
 
 app = FastAPI()
 
+origins = [
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+    "http://138.232.66.68:5173",
+]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
