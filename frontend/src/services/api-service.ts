6ec018d85170befeb7ba4122e25a9b3f55b573aa
JoeTheHammer <hammererjohannes@gmail.com>
2025-11-13 09:26:08 +0100

    Fixed dockerfile, added test experiments

--- frontend/src/services/api-service.ts
+++ frontend/src/services/api-service.ts
@@ -1,5 +1,7 @@
 const defaultHost = '127.0.0.1'
 
 export const getBaseHost = (): string => {
-  return 'http://' + import.meta.env.VITE_BACKEND_URL || defaultHost
+  return import.meta.env.VITE_BACKEND_URL
+    ? 'http://' + import.meta.env.VITE_BACKEND_URL
+    : 'http://' + defaultHost
 }
