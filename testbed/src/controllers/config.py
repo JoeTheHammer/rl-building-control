f6c80e4c8f5d1623530fd156860359f6a0abcd91
JoeTheHammer <hammererjohannes@gmail.com>
2025-11-25 17:40:59 +0100

    Added possibility for nested hp

--- testbed/src/controllers/config.py
+++ testbed/src/controllers/config.py
@@ -1,6 +1,6 @@
 from typing import Optional, Any, Dict
 
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, field_validator
 
 
 class Training(BaseModel):
@@ -31,3 +31,32 @@ class RLControllerConfig(BaseModel):
     hyperparameter_tuning: Optional[HyperparameterTuning] = None
     environment_wrapper: EnvironmentWrapper = Field(default_factory=EnvironmentWrapper)
     hyperparameters: Dict[str, Any] = Field(default_factory=dict)
+
+    @field_validator("hyperparameters", mode="before")
+    def parse_dot_notation(cls, value: Dict[str, Any]) -> Dict[str, Any]:
+        """
+        Parses keys with dots into nested dictionaries.
+        """
+
+        new_hyperparams = {}
+
+        for k, v in value.items():
+            if "." in k:
+                keys = k.split(".")
+                current_level = new_hyperparams
+
+                for sub_key in keys[:-1]:
+                    if sub_key not in current_level:
+                        current_level[sub_key] = {}
+                    current_level = current_level[sub_key]
+
+                    if not isinstance(current_level, dict):
+                        raise ValueError(
+                            f"Conflict at key '{sub_key}'. It is already defined as a non-dict value."
+                        )
+
+                current_level[keys[-1]] = v
+            else:
+                new_hyperparams[k] = v
+
+        return new_hyperparams
